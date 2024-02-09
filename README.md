4a8b61d8d921ecca0daefc0bbc3da0681f50b126
Bruno <brunohenriquehns@gmail.com>
2024-02-09 18:14:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Notes
+
+Aplicação desenvolvida durante o NLW Expert da [Rocketseat](https://www.rocketseat.com.br/?utm_source=google&utm_medium=cpc&utm_campaign=lead&utm_term=perpetuo&utm_content=institucional-lead-home-texto-lead-brandkws-none-none-institucional-none-none-br-google&utm_term=rocketseat&utm_campaign=PROGRAMAS-ALL-BRANDKWS-SEM&utm_source=adwords&utm_medium=cpc&hsa_acc=8545075154&hsa_cam=16048648686&hsa_grp=135825188594&hsa_ad=579096962131&hsa_src=g&hsa_tgt=kwd-679159515078&hsa_kw=rocketseat&hsa_mt=b&hsa_net=adwords&hsa_ver=3&gad_source=1&gclid=CjwKCAiAt5euBhB9EiwAdkXWO7GuMQ4TPepqT_VmVur4REAUT9vAf617OSaINkGUpeUauFm6guKbkxoCwTAQAvD_BwE) que converterá automaticamente notas de áudio em texto com React
+
+![Notes](.github/preview.png)
+
+## Índice
+
+- [Ferramentas](#ferramentas)
+- [Instalação](#instalação)
+- [Projeto](#projeto)
+- [Layout](#layout)
+- [Licença](#licença)
+
+## Ferramentas
+
+- [Vite](https://vitejs.dev/) v5.0.8
+- TypeScript v5.2.2
+- [Tailwind CSS](https://tailwindcss.com/) v3.4.1
+- [React](https://react.dev/) v18.2.0
+- [Radix UI](https://www.radix-ui.com/) v1.0.5
+- SpeechRecognition API
+
+## Instalação
+
+Siga as etapas abaixo para configurar e instalar o Notes em seu ambiente local:
+
+1. Clone o repositório e acesse o diretório:
+
+```bash
+git clone git@github.com:brunohnsouza/nlw-expert.git
+cd nlw-expert
+```
+
+2. Instale as dependências do projeto:
+
+```bash
+npm install
+```
+
+3. Inicie o servidor em modo de desenvolvimento:
+
+```bash
+npm run dev
+```
+
+Acesse http://localhost:5173 para visualizar a aplicação.
+
+## Projeto
+
+Veja o projeto online [clicando aqui]()
+
+## Layout
+
+[Figma](https://www.figma.com/community/file/1336456128647909148) do projeto
+
+## Licença
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/app.tsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import logo from "./assets/logo-nlw-expert.svg";
+import { NewNoteCard } from "./components/new-note-card";
+import { NoteCard } from "./components/note-card";
+
+interface Note {
+  id: string;
+  date: Date;
+  content: string;
+}
+
+export function App() {
+  const [search, setSearch] = useState("");
+  const [notes, setNotes] = useState<Note[]>(() => {
+    return JSON.parse(localStorage.getItem("notes") || "[]");
+  });
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    };
+
+    const notesArray = [newNote, ...notes];
+
+    setNotes(notesArray);
+
+    localStorage.setItem("notes", JSON.stringify(notesArray));
+  }
+
+  function onNoteDeleted(id: string) {
+    const notesArray = notes.filter((note) => note.id !== id);
+
+    setNotes(notesArray);
+
+    localStorage.setItem("notes", JSON.stringify(notesArray));
+  }
+
+  function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value);
+  }
+
+  const fielteredNotes =
+    search !== ""
+      ? notes.filter((note) =>
+          note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+        )
+      : notes;
+
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
+      <img src={logo} alt="NLW Expert" />
+
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em sus notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+          onChange={handleSearch}
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[250px] gap-6">
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+
+        {fielteredNotes.map((note) => {
+          return (
+            <NoteCard onNoteDeleted={onNoteDeleted} key={note.id} note={note} />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
